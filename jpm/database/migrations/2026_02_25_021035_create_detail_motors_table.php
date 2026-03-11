b77c256d0927d0708a73947703146cc6402cd4da
SapaKirian <syavaargiean123@gmail.com>
2026-03-11 12:48:39 +0700

    update error

--- jpm/database/migrations/2026_02_25_021035_create_detail_motors_table.php
+++ jpm/database/migrations/2026_02_25_021035_create_detail_motors_table.php
@@ -9,35 +9,35 @@ return new class extends Migration {
      * Run the migrations.
      */
     public function up()
-{
-    Schema::create('detail_motor', function (Blueprint $table) {
-        $table->id();
-        // Menghubungkan ke tabel motor
-        $table->foreignId('motor_id')->constrained('motor')->onDelete('cascade');
-        
-        // Data Spesifikasi
-        $table->text('mesin');
-        $table->text('rangka');
-        $table->text('dimensi');
-        $table->text('kelistrikan');
-        
-        // Tambahan untuk Varian dan Harga (Disatukan)
-        $table->text('daftar_varian')->comment('Contoh isi: Hybrid S, Hybrid');
-        $table->text('daftar_harga')->comment('Contoh isi: 18900000, 18200000');
-        
-        // Tambahan untuk Warna (Opsional tapi bagus untuk detail)
-        $table->text('daftar_warna')->nullable()->comment('Contoh isi: Dark Grey, Silver');
+    {
+        Schema::create('detail_motor', function (Blueprint $table) {
+            $table->id();
+          
+            $table->foreignId('motor_id')->constrained('motor')->onDelete('cascade');
 
-        $table->string('video_url')->nullable();
-        $table->timestamps();
-    });
-}
+          
+            $table->text('mesin');
+            $table->text('rangka');
+            $table->text('dimensi');
+            $table->text('kelistrikan');
+
+        
+            $table->text('daftar_varian')->comment('Contoh isi: Hybrid S, Hybrid');
+            $table->text('daftar_harga')->comment('Contoh isi: 18900000, 18200000');
+
+           
+            $table->text('daftar_warna')->nullable()->comment('Contoh isi: Dark Grey, Silver');
+
+            $table->string('video_url')->nullable();
+            $table->timestamps();
+        });
+    }
 
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
-        Schema::dropIfExists('detail_motors');
+        Schema::dropIfExists('detail_motor'); 
     }
 };
